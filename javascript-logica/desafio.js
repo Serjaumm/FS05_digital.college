4d839c40d89073e9dd55f51c597e0f9b6aebd19d
Sergio Daniel <daniel.sergio808@gmail.com>
2025-07-24 07:33:47 -0300

    fiz parte do desafio ja

---
+++ javascript-logica/desafio.js
@@ -0,0 +1,22 @@
+//crie um mercantil funcional, com um array chamado mercantil que tenha objeots reperesentando produtos com nome, preço e quantidade em estoque.
+//crie um objeto chamado caixa que tenha as propriedades saldo, produtosVendidos e metodos como vendaProduto, contar estoque, valorEmCaixa.
+
+const mercantil = [
+      {nome: "Arroz", preco: 4.00, estoque: 50},
+      {nome: "Feijão", preco: 5.00, estoque: 30},
+      {nome: "Macarrão", preco: 3.00, estoque:35},
+      {nome: "Óleo", preco: 8.00, estoque: 20},
+      {nome: "Açúcar", preco: 4.50, estoque: 40},
+      {nome: "Biscoito", preco: 2.00, estoque: 60},
+      {nome: "Leite", preco: 3.50, estoque: 25},
+];
+const caixa ={
+   saldo: 0,
+   produtosVendidos: 0,
+
+   vendaProduto(nome, preco, quantidade) {
+      for (let i = 0; i < mercantil.length; i++) {
+            if mercanitl 
+      }
+}
+}
